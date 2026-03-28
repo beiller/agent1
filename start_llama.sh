02dbdacff1c619eb761248b8c8272697a6790ceb
Bill <beiller@gmail.com>
2026-03-28 12:49:53 -0700

    Maybe this issue,

--- start_llama.sh
+++ start_llama.sh
@@ -15,7 +15,5 @@ LLAMA_MODELS_DIR="${LLAMA_MODELS_DIR:-./models}"
 
 "$LLAMA_SERVER_PATH" \
     --models-dir "$LLAMA_MODELS_DIR" \
-    --ctx-size 65535 --temp 0.6 --top-p 0.95 --top-k 20 --min-p 0 \
-    --presence-penalty 1.5 --jinja -t 8 --n-gpu-layers 99 \
-    --flash-attn on --cache-reuse 512 --fit on -b 4096 -ub 1024 --parallel 1 \
+    --parallel 1 \
     --host 0.0.0.0 --port 8080
